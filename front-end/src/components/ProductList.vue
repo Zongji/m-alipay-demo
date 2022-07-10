c806ee3bb4928e11771b96a9e12d1a378bc5dd4e
liangzongji <liangzongji@gmail.com>
2022-07-10 19:20:01 +0800

    update front

---
+++ front-end/src/components/ProductList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="product-list">
+
+    </div>    
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: 'ProductList',
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      list:[]
+    }
+  },
+  created() {
+    axios.get("/api/product/products")
+    .then(res => {
+        console.info(res.data);
+    }).catch(err => {
+        console.error(err);
+    })
+
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
