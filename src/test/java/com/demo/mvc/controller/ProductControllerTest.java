840f80d2d0c8f06daf538ae28f7c1ab205f31a39
liangzongji <liangzongji@gmail.com>
2022-10-11 23:00:30 +0800

    controller测试

---
+++ src/test/java/com/demo/mvc/controller/ProductControllerTest.java
@@ -0,0 +1,49 @@
+package com.demo.mvc.controller;
+
+import com.demo.mvc.BaseTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.*;
+
+@Slf4j
+public class ProductControllerTest extends BaseTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void list() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/product/list")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("page","1")
+                        .param("pageSize","10")
+        ).andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        log.info(mvcResult.getResponse().toString());
+    }
+
+    @Test
+    public void detail() {
+
+    }
+}
